7c0c99dff3c9b1cb25854fb78d94f46ea1d9a491
Anton Zhadan <86596115+anton-zhadan@users.noreply.github.com>
2021-10-07 09:21:38 +0300

    V1

---
+++ script.js
@@ -0,0 +1,25 @@
+// userName = prompt('Enter name').trim().replaceAll(' ','');
+// console.log(name);
+
+// email = prompt('Enter email').toLowerCase().replaceAll(' ','');
+// console.log(email);
+// if(!email.startsWith('@') && !email.endsWith('@') && email.includes('@')){ 
+// 	console.log('Hello')
+// } else{
+// 	console.log('not valid');
+// }
+year = prompt('Enter your year').replaceAll(' ','');
+age = getFullYear - year;
+console.log(age);
+
+
+
+// if (email.includes('@')){ // true && true
+// 	console.log('Hello')
+// } else if (email.startsWith('@')) {
+// 	console.log('not valid');
+// }
+
+// q1 = prompt("Do you want fruits?").toLowerCase().replaceAll(' ','');
+// !email.endsWith('@')
+// console.log(q1);
