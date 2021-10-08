5c8bd8f1b046da15e9d1fe96205e5bd9b45a4d17
Anton Zhadan <86596115+anton-zhadan@users.noreply.github.com>
2021-10-08 15:52:20 +0300

    V2

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// userName = prompt(`Enter name`);
-// userName = userName ? userName.trim() : 'not valid';
-// console.log(userName);
+userName = prompt(`Enter name`);
+userName = userName ? userName.trim() : 'not valid';
+console.log(userName);
 
-// email = prompt ('Enter email');
+email = prompt ('Enter email').trim().toLowerCase();
 
-// if (email.startsWith(`@`)) {
-//     email = `not valid email <b>${email}</b> (symbol @ find in first place)`;
-// } else if (email.endsWith(`@`)) {
-//     email = `not valid email <b>${email}</b> (symbol @ find in last place)`;
-// } else if (!email.includes(`@`)) {
-//     email = `not valid email <b>${email}</b> (symbol @ not exist)`;
-// }
+if (email.startsWith(`@`)) {
+    email = `not valid email <b>${email}</b> (symbol @ find in first place)`;
+} else if (email.endsWith(`@`)) {
+    email = `not valid email <b>${email}</b> (symbol @ find in last place)`;
+} else if (!email.includes(`@`)) {
+    email = `not valid email <b>${email}</b> (symbol @ not exist)`;
+}
 
-// console.log (email);
+console.log (email);
 
-DOB = prompt(`Enter year`);
+DOB = prompt(`Enter year`);  //DOB - day of birthday
 age = '';
 
 if (!isNaN(DOB)) {
-    age = new Date().getFullYear()-DOB; 
+    age = new Date().getFullYear() - DOB; 
 }
 
 console.log (age);
 
-document.write (`<li>Age: <b>${age}</b></li>`)
+document.write (`<li>Full name: <b>${userName}</b></li> <li>Email: ${email}</li> <li>Age: <b>${age}</b></li>`);
